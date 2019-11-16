00fcdf1be4515738bcd79e70bfa462863c594249
wangmk <wangmk@umich.edu>
2019-11-16 10:40:19 -0500

    Sketch out the skeleton of the project.

---
+++ R/main.R
@@ -0,0 +1,16 @@
+
+varselect = function(x,...){
+  UseMethod("varselect",x)
+}
+
+varselect.default = function(x, y, nvmax = 8,
+                             method=c("forward", "backward")){
+  ncov = dim(x)[2] # number of covariates
+  rval = switch(1+pmatch(method[1],
+                         c("forward", "backward"),
+                         nomatch=0),
+                stop(paste("Ambiguous or unrecognised method name :",method)),
+                fselect(x, y, nvmax, ncov),
+                bselect(x, y, nvmax, ncov))
+}
+
