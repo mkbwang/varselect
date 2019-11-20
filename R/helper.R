48dea1e9faa80f5c19d11f97650b96360f711fe7
wangmk <wangmk@umich.edu>
2019-11-19 22:02:59 -0500

    Using Roxygen to generate help document.

--- R/helper.R
+++ R/helper.R
@@ -1,14 +1,21 @@
+
+
+#' @export
 print.summary.varselect = function(x, ...){
   cat("The variable selection order is \n")
   cat(paste(x, collapse=" "))
 }
 
+
+#' @export
 summary.varselect <- function(object,...){
   rval = object$variables
   class(rval) <- "summary.varselect"
   rval
 }
 
+
+#' @export
 coef.varselect <- function(object, id, ...){
   vars = c("Intercept", object$variables[1:id])
   coefs = object$coefs[1:(id+1), id]
@@ -16,13 +23,15 @@ coef.varselect <- function(object, id, ...){
   coefs
 }
 
+
+#' @export
 plot.varselect <- function(object, ...){
   Cp = as.vector(object$Cp)
   AIC = as.vector(object$AIC)
   BIC = as.vector(object$BIC)
   adjR2 = as.vector(object$adjR2)
   criteria = data.frame(Cp, AIC, BIC, adjR2)
-  criteria$Order = c(1,2,3,4,5,6,7,8)
+  criteria$Order = c(1:length(Cp))
   criteria = melt(criteria, id.vars=c("Order"), variable.name="Score")
   
   p <- ggplot(criteria, aes(Order, value)) + geom_line() + geom_point(size=1.2)
--- R/main.R
+++ R/main.R
@@ -1,26 +1,31 @@
-
+#' Varselect
+#' 
+#' Forward selection for linear regression
+#'
+#' @import Rcpp ggplot2 reshape2
+#' @importFrom Rcpp evalCpp
+#' 
+#' @param x Design matrix or model formula for full model
+#' @param y Response vector
+#' @param data Optional data frame
+#' @param nvmax Maximal number of parameters included in the subset model
+#' @return A list with multiple fields. Users can apply \code{summary}, \code{coef} and \code{plot} to explore the results. 
+#' @examples
+#' data(Hitters)
+#' mymodel = varselect(Salary~., data=Hitters, nvmax=12)
+#' summary(mymodel)
+#' coef(mymodel, id=6)
+#' plot(mymodel)
+#' @useDynLib varselect
+#' 
+#' @rdname varselect
+#' @export varselect
 varselect = function(x,...){
   UseMethod("varselect",x)
 }
 
-# by default it receives the xmatrix, ymatrix and nvmax
-varselect.default = function(x, y, nvmax = 8){
-  ncov = dim(x)[2] # number of covariates
-  covariates = colnames(x)
-  x = as.matrix(x)
-  y = as.matrix(y)
-  
-  rval = fselect(x, y, nvmax, ncov)
-  
-  rval$variables = as.vector(rval$variables)
-  if (!is.null(covariates)){
-    rval$variables = covariates[rval$variables]
-  }
-  class(rval)<-"varselect"
-  invisible(rval)
-}
-
-# it also receives formula and data as first two parameters
+#' @rdname varselect
+#' @export
 varselect.formula = function(fml, data, nvmax = 8){
   response_var = as.character(fml)[2]
   y = data[, response_var, drop=F]
@@ -37,5 +42,24 @@ varselect.formula = function(fml, data, nvmax = 8){
     rval$variables = covariates[rval$variables]
   }
   class(rval) <- "varselect"
+  invisible(rval)
+}
+
+
+#' @rdname varselect
+#' @export
+varselect.default = function(x, y, nvmax = 8){
+  ncov = dim(x)[2] # number of covariates
+  covariates = colnames(x)
+  x = as.matrix(x)
+  y = as.matrix(y)
+  
+  rval = fselect(x, y, nvmax, ncov)
+  
+  rval$variables = as.vector(rval$variables)
+  if (!is.null(covariates)){
+    rval$variables = covariates[rval$variables]
+  }
+  class(rval)<-"varselect"
   invisible(rval)
 }
